890ad7ce9224ff377aa4bb3513f437e91aae16e2
Andrea <oquendo.3455@gmail.com>
2020-12-10 21:18:20 -0300

    banner done

--- js/banner.js
+++ js/banner.js
@@ -1,1 +1,103 @@
-//document.getElementById("banner-pic").setAttribute("src", "images/banner/skye.png");
+function addPageIndex(){
+    var num=0;
+    
+    _webtoonBannerList.forEach(webtoon => {
+        var newid= "pic"+num;
+        var miniball = "circle"+num;
+        var newtext= "<a id='"+miniball+ "' class='page-circle' href='javascript:void(0)'></a>"
+        num++;
+        innerText+=newtext;
+        newtext= '<img class="banner-pic" id="'+newid+'" src="'+webtoon.bannerpic+ '" style="display:none;" />'
+        innerBanner+=newtext;
+    });
+}
+
+function intervalBanner(){
+    i++;
+    var index = i%(_webtoonBannerList.length);
+    
+    
+    showBanner(index);
+}
+
+function showBanner(index){
+    var pic="#pic" + index;
+    changeLink(index);
+    if(index == 0) {
+        hideBanner(_webtoonBannerList.length -1);
+    }
+    else{
+        hideBanner(index - 1);
+    }
+
+    changeBallColorWhite(index);
+    $(pic).fadeIn("slow");
+}
+
+function clickShowBanner(last, index){
+    var pic="#pic" + index;
+    changeLink(index);
+    hideBanner(last);
+    changeBallColorWhite(index);
+    $(pic).fadeIn("slow");
+}
+
+function hideBanner(index){
+    reverseBallColor(index);
+    var pic="#pic" + index;
+    $(pic).fadeOut("slow");
+}
+
+function changeLink(index)
+{
+    _linkBox.setAttribute("href", _webtoonBannerList[index].link);
+}
+
+function changeBallColorWhite(index){
+    var pic="circle"+index;
+    document.getElementById(pic).classList.add("open");
+}
+
+function reverseBallColor(index){
+    var pic="circle"+index;
+    document.getElementById(pic).classList.remove("open");
+}
+
+function intervalism(){
+    _interval = setInterval(intervalBanner, _time);
+}
+
+function circlesEvents(){
+    _webtoonBannerList.forEach(function(webtoon, index){
+        var name = "circle" + index;
+    
+        document.getElementById(name).addEventListener("click", function(){
+            clearInterval(_interval);
+            last=i%_webtoonBannerList.length;
+            console.log("last= " + last + " index= " + index + " i= " + i);
+            i=index;
+            
+            clickShowBanner(last, index);
+            intervalism();
+        })
+    });  
+    
+}
+
+function initBanner(){
+    addPageIndex();
+    _pagination.innerHTML= innerText;
+    _bannerPics.innerHTML=innerBanner;
+    intervalBanner();
+    circlesEvents();
+    intervalism();
+}
+
+initBanner();
+
+
+
+
+
+
+
--- js/webtoon-info.js
+++ js/webtoon-info.js
@@ -8,106 +8,12 @@ _notshoujo= new Webtoon("images/banner/notshoujo.png", "https://www.webtoons.com
 _truebeauty= new Webtoon("images/banner/truebeauty.png", "https://www.webtoons.com/en/romance/truebeauty/list?title_no=1436");
 _skye= new Webtoon("images/banner/skye.png", "https://www.webtoons.com/en/fantasy/skye/list?title_no=2234");
 
-_webtoonBannerList = [_empress, _notshoujo, _truebeauty,_skye];
-
+var _webtoonBannerList = [_empress, _notshoujo, _truebeauty,_skye];
 var innerText=" ";
 var innerBanner=" "
-
-function addPageIndex(){
-    var num=0;
-    
-    _webtoonBannerList.forEach(webtoon => {
-        var newid= "pic"+num;
-        var miniball = "circle"+num;
-        var newtext= "<a id='"+miniball+ "' class='page-circle' href='javascript:void(0)'></a>"
-        num++;
-        innerText+=newtext;
-        newtext= '<img class="banner-pic" id="'+newid+'" src="'+webtoon.bannerpic+ '" style="display:none;" />'
-        innerBanner+=newtext;
-    });
-}
-
-function intervalBanner(){
-    i++;
-    var index = i%(_webtoonBannerList.length);
-    
-    
-    showBanner(index);
-}
-
-function showBanner(index){
-    var pic="#pic" + index;
-    changeLink(index);
-    if(index == 0) {
-        hideBanner(_webtoonBannerList.length -1);
-    }
-    else{
-        hideBanner(index - 1);
-    }
-
-    changeBallColorWhite(index);
-    $(pic).fadeIn("slow");
-}
-
-function clickShowBanner(last, index){
-    var pic="#pic" + index;
-    changeLink(index);
-    hideBanner(last);
-    changeBallColorWhite(index);
-    $(pic).fadeIn("slow");
-}
-
-function hideBanner(index){
-    reverseBallColor(index);
-    var pic="#pic" + index;
-    $(pic).fadeOut("slow");
-}
-
-function changeLink(index)
-{
-    _linkBox.setAttribute("href", _webtoonBannerList[index].link);
-}
-
-function changeBallColorWhite(index){
-    var pic="circle"+index;
-    document.getElementById(pic).classList.add("open");
-}
-
-function reverseBallColor(index){
-    var pic="circle"+index;
-    document.getElementById(pic).classList.remove("open");
-}
-
-addPageIndex();
-let _pagination = document.getElementById("pagination").innerHTML= innerText;
-let _bannerPics = document.getElementById("poster-images").innerHTML=innerBanner;
-let _linkBox = document.getElementsByClassName("link-box")[0];
-
 var i=-1;
 var _time = 3000;
-intervalBanner();
-circlesEvents();
-intervalism();
 var _interval;
-console.log(i);
-function intervalism(){
-    _interval = setInterval(intervalBanner, _time);
-}
-function circlesEvents(){
-    _webtoonBannerList.forEach(function(webtoon, index){
-        var name = "circle" + index;
-    
-        document.getElementById(name).addEventListener("click", function(){
-            clearInterval(_interval);
-            last=i%_webtoonBannerList.length;
-            console.log("last= " + last + " index= " + index + " i= " + i);
-            i=index;
-            
-            clickShowBanner(last, index);
-            intervalism();
-        })
-    });  
-    
-}
-
- 
+let _pagination = document.getElementById("pagination");
+let _bannerPics = document.getElementById("poster-images");
+let _linkBox = document.getElementsByClassName("link-box")[0];
