0a420731c98e8939f89fcecf899c3702392de780
Andrea <oquendo.3455@gmail.com>
2020-12-12 19:29:40 -0300

    weekly bar almost done

--- js/weekly-update-head.js
+++ js/weekly-update-head.js
@@ -11,11 +11,33 @@ let _daysInWeek = {
 }
 
 let _actualClicked = 0;
+let _dummyText = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et...";
+let _dummyAuthor = "authorname";
+let _polkaCatLink = "https://www.youtube.com/watch?v=NUYvbT6vTPs";
 
+let genres = {
+    romance:"romance",
+    scifi:"sci-fi",
+    drama:"drama",
+    comedy:"comedy",
+    slice:"slice of life"
+}
 
+function WebtoonCard(name, author, synopsis, grade, genre, link, image){
+    this.name = name;
+    this.author = author;
+    this.synopsis = synopsis;
+    this.grade = grade;
+    this.genre = genre;
+    this.link = link;
+    this.image = image;
+}
 
+let _cardAcception = new WebtoonCard("Acception", _dummyAuthor, _dummyText,15, genres.romance, _polkaCatLink, "images/stories/acception.jpg");
+let _cardAntiStalker = new WebtoonCard("Anti Stalker", _dummyAuthor, _dummyText,15, genres.romance, _polkaCatLink, "images/stories/antistalker.jpg");
+let _cardDuke = new WebtoonCard("Love Advice from the Great Duke of Hell", _dummyAuthor,15, _dummyText, genres.comedy, _polkaCatLink, "images/stories/duke.jpg");
 
+let _cardsOfDay = [[_cardAcception], [_cardAcception,_cardAntiStalker],[],[],[],[],[]];
 
-
-
+let _pagesBar = document.getElementsByClassName("page-box")[0];
 
--- js/weekly-update.js
+++ js/weekly-update.js
@@ -25,4 +25,68 @@ function init(){
     });
 }
 
+function createDayPage(){
+    var _textInsert =
+    '<ul class="page" style="display:none"></ul>';
+    return _textInsert;
+}
+
+function createCard(webtoonCard){
+    var _textInsert;
+    var _link = "<a href="+ webtoonCard.link+" class='page-flip' target='_blank'>";
+    var image = "<img src="+ webtoonCard.image+ " />";
+    var info = "<div class='info'><p class='title'>"+webtoonCard.name +"</p><span class='grade-area'><img src='images/icons/heart.svg' width=12px />"+webtoonCard.grade+"</p></div>"
+    var genre = "<span class='genre "+ webtoonCard.genre +"'>"+webtoonCard.genre+"</span>";
+    var cardFront = "<div class='card-front'>" + image + info + genre + "</div>";
+    var infoback ="<div class='info'><p class='title'>"+ webtoonCard.name+"</p><p class='author'>"+ webtoonCard.author +"</p><p class='horizontal-line'></p><p class='synopsis'>"+ webtoonCard.synopsis+"</p></div>";
+    var cardBack='<div class="card-back">' + infoback +'</div>';
+    _textInsert = '<li>' + _link +cardFront + cardBack + '</a></li>';
+    /*
+    <li>
+                <a href="https://www.webtoons.com/en/romance/nice-to-meet-you/list?title_no=2066&page=1" class="page-flip" target="_blank">
+                <div class="card-front">
+                  <img src="images/stories/nicetomeetyou.jpg" />
+                  <div class="info">
+                    <p class="title">Nice to Meet You</p>
+                    <span class="grade-area"><img src="images/icons/heart.svg" width=12px /> 14.1K</p>
+                  </div>
+                  <span class="genre romance">Romance</span>
+                </div>
+                <div class="card-back">
+                  <div class="info">
+                    <p class="title">Nice to Meet You</p>
+                    <p class="author">Wishroomness</p>
+                    <p class="horizontal-line"></p>
+                    <p class="synopsis">A ditsy university student Mew finds a lost student card. Instead of doing the sensible thing, Mew decides to let fate take the wheel and try something silly. Little did she know, she would be...</p>
+                  </div>
+                </div>
+              </a>
+              </li>
+    */
+   return _textInsert;
+}
+
+function addPages(){
+    var inner="";
+    _dayArray.forEach(function(){
+        inner+=createDayPage();
+    })
+    _pagesBar.innerHTML = inner;
+}
+
+function addCards(){
+    var day=document.getElementsByClassName("page");
+    _cardsOfDay.forEach(function(list, index){
+        var inner ="";
+        list.forEach(function(webtoon){
+            inner+=createCard(webtoon);
+        })
+        day[index].innerHTML=inner;
+    });
+}
+
+function activatePage(){}
+
+addPages();
+addCards();
 init();
